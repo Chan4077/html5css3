52473c875a689d042f3144ebb9fdd3300b78657d
Edric Chan <edric.chan.1997@gmail.com>
2017-04-07 22:55:06 +0800

    feat() - Fill up rest of content

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 angular.module('HTMLCSSApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'app.config', 'app.directives'])
 .controller('HomeController', function($scope) {
   console.log('Loaded!');
+  $scope.items = [
+    {text:"Test"},
+    {text:"Wow"}
+  ]
+  $scope.doSomething = function(item) {
+    console.log('Item is: '+item);
+  }
 })
